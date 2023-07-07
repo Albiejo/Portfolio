eaf1ed04676bb5b5fecdc9943b805294ba61ba4e
Albiejo <137685784+Albiejo@users.noreply.github.com>
2023-07-07 10:16:42 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,354 @@
+
+
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Intro type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
+
+
+/*
+function validateform()
+{
+var name=document.getElementById("fname").value;
+var email=document.getElementById("email").value;
+var msg=document.getElementById("subject").value;
+var phone=document.getElementById("phone").value;
+const numberPattern = /^\d/;      // Define the regex pattern to check for the first character as a number
+
+if
+{
+  
+}
+
+else if(email==null||email=="")
+{
+  alert('EMAIL CANNOT BE BLANK !')
+  return false
+}
+
+else if(msg==null || msg=="")
+{
+  alert('MESSAGE CANNOT BE BLANK.')
+  return false;
+}
+
+// Check if the name starts with a number
+else if (numberPattern.test(name)) {
+   
+    } 
+
+  else if()
+  {
+   
+  }
+
+ 
+ 
+ 
+  else if(isNaN(phone))
+  {
+    alert('PHONE CANNOT CONTAIN LETTERS')
+    return false;
+  }
+
+  
+}
+
+*/
+
+
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var mobilenumber = document.getElementById("mobilenumber").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+ 
+  if (name==null || name==""){
+    alert('NAME CANNOT BE BLANK !')
+  return false;
+  }
+
+  if (/\d/.test(name)){
+    alert('Name cannot not start with a number!');
+    return false;
+  }
+
+  if (mobilenumber=="" || mobilenumber==null) {
+    alert('PHONE CANNOT BE BLANK..')
+    return false;
+  }
+
+  if (isNaN(mobilenumber)){
+    alert('PHONE CANNOT CONTAIN LETTERS')
+    return false;
+  }
+
+  if (mobilenumber.length<10){
+    alert('PHONE MUST CONTAIN 10 DIGITS')
+    return false;
+  }
+
+  if (mobilenumber.length>10){
+    alert('PHONE MUST CONTAIN 10 DIGITS')
+    return false;
+  }
+
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailRegex.test(email)) {
+
+    alert('Email must be a valid email address')
+    return false;
+
+  }
+
+  if (subject == "") {
+    alert('Subject must be filled out')
+    return false;
+  }
+
+
+  if (message == "") {
+    alert('message must be filled out')
+    return false;
+  }
+  
+//checking wether num starts with 7,8,9.....//
+
+  if((mobilenumber.charAt(0)!=9) && (mobilenumber.charAt(0)!=8) && (mobilenumber.charAt(0)!=7))
+  {
+    alert('PHONE MUST START WITH 9 OR 8 OR 7');
+    return false;
+  }
+
+
+
+  return true;
+}
+
+
+
